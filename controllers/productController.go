a60901741521518ae995c5d85e2c401b0edf5a77
eugene <isip_e.e.plaksin@mpt.ru>
2022-11-25 12:42:27 +0300

    Вывод одного товара

--- controllers/productController.go
+++ controllers/productController.go
@@ -2,13 +2,16 @@ package controllers
 
 import (
 	"encoding/json"
+	"github.com/gorilla/mux"
 	"net/http"
 	"storeAPI/dbConnection"
 	"storeAPI/models"
+	"strconv"
 )
 
+var db = dbConnection.DB
+
 func GetAllProducts(w http.ResponseWriter, r *http.Request) {
-	db := dbConnection.DB
 	rows, err := db.Query("select * from product where IsDeleted = 0")
 	if err != nil {
 		panic(err.Error())
@@ -26,5 +29,23 @@ func GetAllProducts(w http.ResponseWriter, r *http.Request) {
 }
 
 func GetOneProduct(w http.ResponseWriter, r *http.Request) {
-
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		json.NewEncoder(w).Encode("Некорректный запрос")
+	}
+	rows, err := db.Query("select * from product where IsDeleted = 0 and ID_Product = $1", id)
+	if err != nil {
+		json.NewEncoder(w).Encode("Такого товара не существует")
+	}
+	var products []models.Product
+	for rows.Next() {
+		var product models.Product
+		err = rows.Scan(&product.IDProduct, &product.ProductName, &product.IsDeleted)
+		if err != nil {
+			panic(err.Error())
+		}
+		products = append(products, product)
+	}
+	json.NewEncoder(w).Encode(products)
 }
