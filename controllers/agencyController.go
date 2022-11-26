56f46bc7d08c54d2f5d2e42d588e957c01a8cbf8
eugene <isip_e.e.plaksin@mpt.ru>
2022-11-26 16:10:38 +0300

    Рекламные агенства

---
+++ controllers/agencyController.go
@@ -0,0 +1,98 @@
+package controllers
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"net/http"
+	"storeAPI/dbConnection"
+	"storeAPI/models"
+	"strconv"
+)
+
+func GetAllAgencies(w http.ResponseWriter, r *http.Request) {
+	db := dbConnection.DB
+	rows, err := db.Query("select * from `agency` where IsDeleted = 0")
+	if err != nil {
+		panic(err.Error())
+	}
+	var agencies []models.Agency
+	for rows.Next() {
+		var agency models.Agency
+		err = rows.Scan(&agency.IDAgency, &agency.AgencyName, &agency.IsDeleted)
+		if err != nil {
+			panic(err.Error())
+		}
+		agencies = append(agencies, agency)
+	}
+	json.NewEncoder(w).Encode(agencies)
+}
+
+func GetOneAgency(w http.ResponseWriter, r *http.Request) {
+	db := dbConnection.DB
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		json.NewEncoder(w).Encode("Некорректный запрос")
+	}
+
+	var agency models.Agency
+
+	if err := db.QueryRow("select * from `agency` where IsDeleted = 0 and ID_Agency = ?", id).Scan(&agency.IDAgency, &agency.AgencyName, &agency.IsDeleted); err != nil {
+		json.NewEncoder(w).Encode(err)
+	}
+	json.NewEncoder(w).Encode(agency)
+}
+
+func AddAgency(w http.ResponseWriter, r *http.Request) {
+	db := dbConnection.DB
+	if r.Body == nil {
+		json.NewEncoder(w).Encode("Поля ввода не заполнены")
+	}
+	agencyName := r.FormValue("agency_name")
+
+	//Валидатор
+	query := "call Agency_Insert(?)"
+	res, err := db.ExecContext(context.Background(), query, agencyName)
+	if err != nil {
+		json.NewEncoder(w).Encode(err)
+	}
+	json.NewEncoder(w).Encode(res)
+}
+
+func UpdateAgency(w http.ResponseWriter, r *http.Request) {
+	db := dbConnection.DB
+	if r.Body == nil {
+		json.NewEncoder(w).Encode("Поля ввода не заполнены")
+	}
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		json.NewEncoder(w).Encode("Некорректный запрос")
+	}
+	agencyName := r.FormValue("agency_name")
+
+	//Валидатор
+
+	query := "call Agency_Update(?,?)"
+	res, err := db.ExecContext(context.Background(), query, id, agencyName)
+	if err != nil {
+		json.NewEncoder(w).Encode(err)
+	}
+	json.NewEncoder(w).Encode(res)
+}
+
+func DeleteAgency(w http.ResponseWriter, r *http.Request) {
+	db := dbConnection.DB
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		json.NewEncoder(w).Encode("Некорректный запрос")
+	}
+	query := "call Agency_Delete(?)"
+	res, err := db.ExecContext(context.Background(), query, id)
+	if err != nil {
+		json.NewEncoder(w).Encode(err)
+	}
+	json.NewEncoder(w).Encode(res)
+}
--- main.go
+++ main.go
@@ -14,10 +14,22 @@ func main() {
 	r.Handle("/api", controllers.ValidateJWT(controllers.GetPosts))
 	r.HandleFunc("/jwt", controllers.GetJWT)
 	r.HandleFunc("/posts", controllers.GetPosts)
+
+	//Товары
 	r.HandleFunc("/products", controllers.GetAllProducts).Methods("GET")
 	r.HandleFunc("/products/add", controllers.AddProduct).Methods("POST")
 	r.HandleFunc("/products/edit/{id}", controllers.UpdateProduct).Methods("POST")
 	r.HandleFunc("/products/{id}", controllers.GetOneProduct).Methods("GET")
 	r.HandleFunc("/products/delete/{id}", controllers.DeleteProduct).Methods("GET")
+
+	//Рекламные агенства
+	r.HandleFunc("/agencies", controllers.GetAllAgencies).Methods("GET")
+	r.HandleFunc("/agencies/add", controllers.AddAgency).Methods("POST")
+	r.HandleFunc("/agencies/edit/{id}", controllers.UpdateAgency).Methods("POST")
+	r.HandleFunc("/agencies/{id}", controllers.GetOneAgency).Methods("GET")
+	r.HandleFunc("/agencies/delete/{id}", controllers.DeleteAgency).Methods("GET")
+
+	//Поставщики
+
 	log.Fatal(http.ListenAndServe("localhost:4000", r))
 }
